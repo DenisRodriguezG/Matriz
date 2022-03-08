1d6924cf1c5685b726cd2e2e24e525fd7b873c22
Denis Urías <78060754+DenisRodriguez-17@users.noreply.github.com>
2022-03-07 21:19:09 -0600

    Agregar archivos de proyecto.

---
+++ Matriz/Matriz.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz
+{
+    internal class Matriz
+    {
+        private int[,] matriz;
+        private int row;
+        private int column;
+        public Matriz(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+            matriz = new int[row, column];
+        }
+
+        public void insert()
+        {
+            Random R1 = new Random();
+            for (int i = 0; i < row; i++)
+                for (int j = 0; j < column; j++)
+                    matriz[i, j] = R1.Next(0, 100);
+        }
+        public void printMatriz()
+        {
+            Console.WriteLine(this);
+        }
+        public override string ToString()
+        {
+            string character = "";
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                    character += matriz[i, j] + " ";
+                character += "\n";
+            }
+                
+            return string.Format(character);
+        }
+    }
+}
---
+++ Matriz/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string []args)
+        {
+            Matriz M1 = new Matriz(3, 2);
+            M1.insert();
+            M1.printMatriz();
+            int[,] N = { { 2, 3 }, { 5, 6 }, { 8, 6 } };
+
+        }
+    }
+}
